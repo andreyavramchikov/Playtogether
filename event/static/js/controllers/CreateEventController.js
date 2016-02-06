0024a8ac292ba400e26be9bf9a745406ae864030
andrey <andrey@student_jobofferusa.com>
2016-02-06 14:52:56 +0300

    requirements + dropdown directive cleanup

--- event/static/js/controllers/CreateEventController.js
+++ event/static/js/controllers/CreateEventController.js
@@ -1,21 +1,27 @@
 var app = angular.module('playTogether');
 
-app.controller('CreateEventController', function ($scope, $http, $timeout, EventService, ActivityService, PlaceService) {
+app.controller('CreateEventController', function ($scope, $rootScope, $http, $state, $timeout, EventService, ActivityService, PlaceService) {
+    $scope.selectedActivity = [];
+
+    //to broadcast event of click into dropdown directive - to close dropdown on the page after clicking on any position;
+    $('#create-event').on("click", function (e) {
+        $rootScope.$broadcast("documentClicked", angular.element(e.target));
+    });
+
     var getPostData = function () {
         var data = {
-            activity: $scope.selectedActivity,
+            activity: $scope.selectedActivity.id,
             start_date: $scope.start_date,
             min_people: $scope.min_people
         };
         return data;
     };
-
-    //$("#dtBox").DateTimePicker();
+    $("#dtBox").DateTimePicker();
     $scope.errors = {};
     $scope.createEvent = function () {
         var data = getPostData();
         EventService.createEvent(data).then(function (response) {
-            console.log(response);
+            $state.go('events');
         }, function (response) {
             var errors = response.data;
             if (errors) {
@@ -35,3 +41,47 @@ app.controller('CreateEventController', function ($scope, $http, $timeout, Event
     });
 });
 
+
+//Directive to create custom beautiful dropdown
+app.directive("dropdown", function ($rootScope) {
+    return {
+        restrict: "E",
+        templateUrl: "/static/pages/utils/dropdown.html",
+        scope: {
+            placeholder: "@",
+            list: "=",
+            selected: "=",
+            property: "@"
+        },
+        link: function (scope) {
+            scope.listVisible = false;
+            scope.isPlaceholder = true;
+
+            scope.select = function (item) {
+                scope.isPlaceholder = false;
+                scope.selected = item;
+            };
+
+            scope.isSelected = function (item) {
+                return item[scope.property] === scope.selected[scope.property];
+            };
+
+            scope.show = function () {
+                scope.listVisible = true;
+            };
+
+            $rootScope.$on("documentClicked", function (inner, target) {
+                console.log($(target[0]).is(".dropdown-display.clicked") || $(target[0]).parents(".dropdown-display.clicked").length > 0);
+                if (!$(target[0]).is(".dropdown-display.clicked") && !$(target[0]).parents(".dropdown-display.clicked").length > 0)
+                    scope.$apply(function () {
+                        scope.listVisible = false;
+                    });
+            });
+
+            scope.$watch("selected", function (value) {
+                scope.isPlaceholder = scope.selected[scope.property] === undefined;
+                scope.display = scope.selected[scope.property];
+            });
+        }
+    }
+});
